7fb6eb36a9a477b656fc1ae134bf300c858d8299
Simon Duc <ducsimon@gmail.com>
2022-03-15 00:43:25 +0100

    modified the burger menu and added some sources

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+/* Sélection des éléments HTML */
+let link = document.getElementById('link')
+let burger = document.getElementById('burger')
+let ul = document.querySelector('ul')
 
+/* gestionnaire d'événement sur le a#link pour venir changer l'attribution de la classe .open à la ul et au span#burger */
+link.addEventListener('click', function(e) {
+  e.preventDefault()
+  burger.classList.toggle('open')
+  ul.classList.toggle('open')
+})
