671aa4940312b9acd3ff1ab935b32c82b990ae05
Simon Duc <ducsimon@gmail.com>
2022-03-16 12:12:38 +0100

    renamed my classes and improved some style properties

--- script.js
+++
@@ -1,5 +0,0 @@
-// const dropdownBtn = document.querySelector(".dropdown-btn");
-// const dropdownMenu = document.querySelector(".dropdown-menu-content");
-// dropdownBtn.addEventListener("click", function () {
-//   dropdownMenu.classList.toggle("visible");
-// });
