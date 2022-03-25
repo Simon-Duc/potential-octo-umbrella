268c76cb512ca2899dcc74f5f652e3030acf4153
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-03-25 11:51:14 +0100

    check media query added js script to make projet part open or close in reponsive design

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function getValue() {
     alert (Math.random() < 0.5);
 }
+
+if (window.matchMedia("(min-width: 1024px)").matches) {
+
+    document.querySelectorAll(".courses-details").forEach(
+        (coursesDetail) => {
+            coursesDetail.setAttribute("open", "")
+        }
+    );
+}
